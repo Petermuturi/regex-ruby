f945db18097833a04941a183495e4811352d9448
Peter Muturi <pmuturi65@gmail.com>
2015-08-18 12:21:10 +0300

    exercise6

---
+++ exercise6.rb
@@ -0,0 +1,10 @@
+#Get rid of duplicates without specifically removing any one value.
+
+nums =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    nums << 11
+    nums.unshift(0)
+  nums.pop[nums.length-1] 
+  nums<< 3
+puts nums.uniq!
+#just added uniq!
+
